06365311f42217d8252c27e3fcf9adea3027d09e
nurhossainfarid <faahsan1516@gmail.com>
2022-05-02 14:37:58 +0600

    add some pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './Pages/Footer/Footer';
+import Header from './Pages/Header/Header';
+import Home from './Pages/Home/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
-      <h3>Start my warehouse project</h3>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/header' element={<Header></Header>}></Route>
+        <Route path='/footer' element={<Footer></Footer>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div>
+            <h3>Home page</h3>
+            <Header></Header>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
